19f01344c04cc7d4a8cb5f106cb9a73aba9d8ae8
saketsn <saketnandan15@gmail.com>
2024-07-24 23:37:41 +0530

    to convert time update

--- src/TimeConvert.java
+++ src/TimeConvert.java
@@ -1,10 +1,16 @@
 public class TimeConvert {
     public static void main(String[] args) {
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(-65,45));
+
         System.out.println(getDurationString(3945));
         System.out.println(getDurationString(65,45));
     }
     public static String getDurationString(int second){
 
+        if(second <0){
+            return "Invalid data for second( " + second + "), must be a positive integer value ";
+        }
         int minutes = second / 60;
         int hours = minutes / 60;
 
@@ -15,6 +21,9 @@ public class TimeConvert {
 
     public static String getDurationString(int minutes,int second){
 
+        if(minutes <0){
+            return "Invalid data for second( " + second + "), must be a positive integer value ";
+        }
         int hours = minutes / 60;
 
         int remainingMinutes = minutes % 60;
