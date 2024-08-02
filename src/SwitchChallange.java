f85bcdd8844fbe7907e3d56085beb939783f54ab
saketsn <saketnandan15@gmail.com>
2024-08-02 09:47:22 +0530

    SWITCH TYPE

---
+++ src/SwitchChallange.java
@@ -0,0 +1,17 @@
+public class SwitchChallange {
+    public static void main(String[] args) {
+
+            char charVariable = 'x';
+
+        switch (charVariable) {
+            case 'A' -> System.out.println(" The word is Able ");
+            case 'B' -> System.out.println(" The word is Baker ");
+            case 'C' -> System.out.println(" The word is Charlie ");
+            case 'D' -> System.out.println(" The word is Dog ");
+            case 'E' -> System.out.println(" The word is Easy ");
+            default -> System.out.println(" The word is not found ");
+        }
+
+
+    }
+}
